12faf3095e70dcf7db0a2bc9cb4567c6ce957a13
MiguelBochiBarros <99305375+MiguelBochiBarros@users.noreply.github.com>
2022-04-25 09:22:41 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import Rodape from "./components/Rodape.vue";
+import Principal from "./components/Principal.vue";
+import Cabecario from "./components/Cabecario.vue";
+import MenuLateral1 from "./components/MenuLateral.vue";
+export default {
+  components: { Rodape, Principal, Cabecario, MenuLateral1 },
+};
+</script>
+<template>
+  <MenuLateral1 />
+  <Cabecario />
+  <Rodape />
+  <Principal />
+</template>
+<style></style>
---
+++ src/components/Principal.vue
@@ -0,0 +1,15 @@
+<script>
+import Bloquinho from "./Bloquinho.vue";
+export default {
+  components: { Bloquinho },
+};
+</script>
+<template>
+  <main id="content">
+    <Bloquinho />
+    <Bloquinho />
+    <Bloquinho />
+    <Bloquinho />
+  </main>
+</template>
+<style></style>
